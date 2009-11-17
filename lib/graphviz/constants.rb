c2edda76f24afae5f60ad5439706065a2312dcc3
glejeune <gregoire.lejeune@free.fr>
2009-11-17 22:03:17 +0100

    Start for 0.9.6

--- lib/graphviz/constants.rb
+++ lib/graphviz/constants.rb
@@ -15,7 +15,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
 
 module Constants
-  RGV_VERSION = "0.9.5"
+  RGV_VERSION = "0.9.6"
   
   ## Const: Output formats
   FORMATS = [
