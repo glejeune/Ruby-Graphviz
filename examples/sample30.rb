ebd8dda8c81da6b2b0c83a77f0e86c565c435eb2
glejeune <gregoire.lejeune@free.fr>
2010-01-14 15:53:06 +0100

    Update ChangeLog

--- examples/sample30.rb
+++ examples/sample30.rb
@@ -3,9 +3,10 @@
 $:.unshift( "../lib" );
 require "graphviz"
 
-GraphViz.new(:G){ |g|
+GraphViz.new(:g){ |g|
   g[:center] = true
   a = g.add_node("A", :shape => "sdl_procedure_start", :peripheries => 0 )
   b = g.add_node("B", :shape => "sdl_save", :peripheries => 0)
-  a << b
+  c = g.add_node("n", :shape => "box", :label => "\\G::\\N\\r")
+  a << b << c
 }.save( :ps => "#{$0}.ps", :extlib => "sdlshapes/sdl.ps" )
