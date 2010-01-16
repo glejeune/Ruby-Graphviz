5e799592b823ff9b7526d95ae1f52c8c25906e7b
glejeune <gregoire.lejeune@free.fr>
2010-01-16 20:00:14 +0100

    Update test suite

--- test/test_init.rb
+++ test/test_init.rb
@@ -2,19 +2,29 @@ require 'test/unit'
 $:.unshift("../lib")
 require 'graphviz'
 
-class TC_MyTest < Test::Unit::TestCase
+class GraphVizTest < Test::Unit::TestCase
   def setup
-    @graph = GraphViz::new( :G )
+    @graph = GraphViz::new( :G )    
   end
 
   # def teardown
   # end
 
   def test_graph
+    assert(@graph, 'Create graph faild.')
+    
+    assert_block 'Set attribut for graph faild.' do
+      @graph['size'] = "10,10"
+    end
+    
+    assert_equal( "\"10,10\"", @graph['size'].to_s, 'Get attribut for graph faild.' )
+    
+    assert_equal( "G", @graph.name, "Wrong graph name." )
+  end
+
+  def test_node
     n, m = nil, nil
     
-    assert(@graph, 'Create graph faild.')
-
     assert_block 'Create node failed.' do
       n = @graph.add_node( "n1" )
     end
@@ -23,6 +33,14 @@ class TC_MyTest < Test::Unit::TestCase
       m = @graph.get_node( "n1" )
     end
 
-    assert_equal( m, n )
+    assert_equal( m, n, "Node corrupted when get." )
+    
+    assert_equal( @graph.node_count, 1, "Wrong number of nodes" )
+    
+    assert_block 'Set attribut for node faild.' do
+      n['label'] = "Hello\n\"world\"\\l"
+    end
+    
+    assert_equal( "\"Hello\\n\\\"world\\\"\\l\"", n['label'].to_s, 'Get attribut for node faild.' )
   end
 end
