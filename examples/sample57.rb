9d18b4912dd78ef7a9f7df3c7e072ac2b33134f4
glejeune <gregoire.lejeune@free.fr>
2010-08-08 23:43:41 +0200

    Update GraphML to GraphViz::GraphML

--- examples/sample57.rb
+++ examples/sample57.rb
@@ -2,5 +2,5 @@ $:.unshift( "../lib" )
 
 require 'graphviz/graphml'
 
-g = GraphML.new( "graphml/cluster.graphml" )
+g = GraphViz::GraphML.new( "graphml/cluster.graphml" )
 g.graph.output( :path => "/usr/local/bin", :png => "#{$0}.png" )
--- lib/graphviz/graphml.rb
+++ lib/graphviz/graphml.rb
@@ -18,199 +18,201 @@
 require 'graphviz'
 require 'rexml/document'
 
-class GraphML
-  attr_reader :attributs
-  attr_accessor :graph
-
-  DEST = {
-    'node'  => [:nodes],
-    'edge'  => [:edges],
-    'graph' => [:graphs],
-    'all'   => [:nodes, :edges, :graphs]
-  }
+class GraphViz
+  class GraphML
+    attr_reader :attributs
+    attr_accessor :graph
   
-  GTYPE = {
-    'directed' => :digraph,
-    'undirected' => :graph
-  }
-  
-  def initialize( xFile )
-    @xmlDoc = REXML::Document::new( File::new( xFile ) )
-    @attributs = {
-      :nodes => {},
-      :edges => {},
-      :graphs => {}
+    DEST = {
+      'node'  => [:nodes],
+      'edge'  => [:edges],
+      'graph' => [:graphs],
+      'all'   => [:nodes, :edges, :graphs]
     }
-    @graph = nil
-    @current_attr = nil
-    @current_node = nil
-    @current_edge = nil
-    @current_graph = nil
     
-    parse( @xmlDoc.root )
-  end
-  
-  def parse( node )
-    #begin
-      send( node.name.to_sym, node )
-    #rescue NoMethodError => e
-    #  raise "ERROR node #{node.name} can be root"
-    #end
-  end
-  
-  def graphml( node )
-    node.each_element( ) do |child|
+    GTYPE = {
+      'directed' => :digraph,
+      'undirected' => :graph
+    }
+    
+    def initialize( xFile )
+      @xmlDoc = REXML::Document::new( File::new( xFile ) )
+      @attributs = {
+        :nodes => {},
+        :edges => {},
+        :graphs => {}
+      }
+      @graph = nil
+      @current_attr = nil
+      @current_node = nil
+      @current_edge = nil
+      @current_graph = nil
+      
+      parse( @xmlDoc.root )
+    end
+    
+    def parse( node )
       #begin
-        send( "graphml_#{child.name}".to_sym, child )
+        send( node.name.to_sym, node )
       #rescue NoMethodError => e
-      #  raise "ERROR node #{child.name} can be child of graphml"
+      #  raise "ERROR node #{node.name} can be root"
       #end
     end
-  end
-  
-  def graphml_key( node )
-    id = node.attributes['id']
-    @current_attr = {
-      :name => node.attributes['attr.name'],
-      :type => node.attributes['attr.type']
-    }    
-    DEST[node.attributes['for']].each do |d|
-      @attributs[d][id] = @current_attr
-    end
     
-    node.each_element( ) do |child|
-      begin
-        send( "graphml_key_#{child.name}".to_sym, child )
-      rescue NoMethodError => e
-        raise "ERROR node #{child.name} can be child of graphml"
+    def graphml( node )
+      node.each_element( ) do |child|
+        #begin
+          send( "graphml_#{child.name}".to_sym, child )
+        #rescue NoMethodError => e
+        #  raise "ERROR node #{child.name} can be child of graphml"
+        #end
       end
     end
     
-    @current_attr = nil
-  end
-  
-  def graphml_key_default( node )
-    @current_attr[:default] = node.texts().to_s
-  end
-  
-  def graphml_graph( node )
-    @current_node = nil
-    
-    if @current_graph.nil?
-      @graph = GraphViz.new( node.attributes['id'], :type => GTYPE[node.attributes['edgedefault']] )
-      @current_graph = @graph
-      previous_graph = @graph
-    else
-      previous_graph = @current_graph      
-      @current_graph = previous_graph.add_graph( node.attributes['id'] )
-    end
-    
-    @attributs[:graphs].each do |id, data|
-      @current_graph.graph[data[:name]] = data[:default] if data.has_key?(:default)
-    end
-    @attributs[:nodes].each do |id, data|
-      @current_graph.node[data[:name]] = data[:default] if data.has_key?(:default)
-    end
-    @attributs[:edges].each do |id, data|
-      @current_graph.edge[data[:name]] = data[:default] if data.has_key?(:default)
-    end
-        
-    node.each_element( ) do |child|
-      #begin
-        send( "graphml_graph_#{child.name}".to_sym, child )
-      #rescue NoMethodError => e
-      #  raise "ERROR node #{child.name} can be child of graphml"
-      #end
-    end
-    
-    @current_graph = previous_graph
-  end
-  
-  def graphml_graph_data( node )
-    @current_graph[@attributs[:graphs][node.attributes['key']][:name]] = node.texts().to_s
-  end
-  
-  def graphml_graph_node( node )
-    @current_node = {}
-
-    node.each_element( ) do |child|
-      case child.name
-      when "graph"
-        graphml_graph( child )
-      else
+    def graphml_key( node )
+      id = node.attributes['id']
+      @current_attr = {
+        :name => node.attributes['attr.name'],
+        :type => node.attributes['attr.type']
+      }    
+      DEST[node.attributes['for']].each do |d|
+        @attributs[d][id] = @current_attr
+      end
+      
+      node.each_element( ) do |child|
         begin
-          send( "graphml_graph_node_#{child.name}".to_sym, child )
+          send( "graphml_key_#{child.name}".to_sym, child )
         rescue NoMethodError => e
           raise "ERROR node #{child.name} can be child of graphml"
         end
       end
+      
+      @current_attr = nil
     end
     
-    unless @current_node.nil?
-      node = @current_graph.add_node( node.attributes['id'] )
-      @current_node.each do |k, v|
-        node[k] = v
+    def graphml_key_default( node )
+      @current_attr[:default] = node.texts().to_s
+    end
+    
+    def graphml_graph( node )
+      @current_node = nil
+      
+      if @current_graph.nil?
+        @graph = GraphViz.new( node.attributes['id'], :type => GTYPE[node.attributes['edgedefault']] )
+        @current_graph = @graph
+        previous_graph = @graph
+      else
+        previous_graph = @current_graph      
+        @current_graph = previous_graph.add_graph( node.attributes['id'] )
       end
+      
+      @attributs[:graphs].each do |id, data|
+        @current_graph.graph[data[:name]] = data[:default] if data.has_key?(:default)
+      end
+      @attributs[:nodes].each do |id, data|
+        @current_graph.node[data[:name]] = data[:default] if data.has_key?(:default)
+      end
+      @attributs[:edges].each do |id, data|
+        @current_graph.edge[data[:name]] = data[:default] if data.has_key?(:default)
+      end
+          
+      node.each_element( ) do |child|
+        #begin
+          send( "graphml_graph_#{child.name}".to_sym, child )
+        #rescue NoMethodError => e
+        #  raise "ERROR node #{child.name} can be child of graphml"
+        #end
+      end
+      
+      @current_graph = previous_graph
     end
     
-    @current_node = nil
-  end
-  
-  def graphml_graph_node_data( node )
-    @current_node[@attributs[:nodes][node.attributes['key']][:name]] = node.texts().to_s
-  end
-  
-  def graphml_graph_node_port( node )
-    @current_node[:shape] = :record
-    port = node.attributes['name']
-    if @current_node[:label]
-      label = @current_node[:label].gsub( "{", "" ).gsub( "}", "" )
-      @current_node[:label] = "#{label}|<#{port}> #{port}"
-    else
-      @current_node[:label] = "<#{port}> #{port}"
-    end
-  end
-  
-  def graphml_graph_edge( node )
-    source = node.attributes['source']
-    source = {source => node.attributes['sourceport']} if node.attributes['sourceport']
-    target = node.attributes['target']
-    target = {target => node.attributes['targetport']} if node.attributes['targetport']
-    
-    @current_edge = @current_graph.add_edge( source, target )
-
-    node.each_element( ) do |child|
-      #begin
-        send( "graphml_graph_edge_#{child.name}".to_sym, child )
-      #rescue NoMethodError => e
-      #  raise "ERROR node #{child.name} can be child of graphml"
-      #end
+    def graphml_graph_data( node )
+      @current_graph[@attributs[:graphs][node.attributes['key']][:name]] = node.texts().to_s
     end
     
-    @current_edge = nil
-  end
-
-  def graphml_graph_edge_data( node )
-    @current_edge[@attributs[:edges][node.attributes['key']][:name]] = node.texts().to_s
-  end
+    def graphml_graph_node( node )
+      @current_node = {}
   
-  def graphml_graph_hyperedge( node )
-    list = []
-    
-    node.each_element( ) do |child|
-      if child.name == "endpoint"
-        if child.attributes['port']
-          list << { child.attributes['node'] => child.attributes['port'] }
+      node.each_element( ) do |child|
+        case child.name
+        when "graph"
+          graphml_graph( child )
         else
-          list << child.attributes['node']
+          begin
+            send( "graphml_graph_node_#{child.name}".to_sym, child )
+          rescue NoMethodError => e
+            raise "ERROR node #{child.name} can be child of graphml"
+          end
         end
       end
+      
+      unless @current_node.nil?
+        node = @current_graph.add_node( node.attributes['id'] )
+        @current_node.each do |k, v|
+          node[k] = v
+        end
+      end
+      
+      @current_node = nil
     end
     
-    list.each { |s|
-      list.each { |t|
-        @current_graph.add_edge( s, t ) unless s == t
-      }
-    } 
+    def graphml_graph_node_data( node )
+      @current_node[@attributs[:nodes][node.attributes['key']][:name]] = node.texts().to_s
+    end
+    
+    def graphml_graph_node_port( node )
+      @current_node[:shape] = :record
+      port = node.attributes['name']
+      if @current_node[:label]
+        label = @current_node[:label].gsub( "{", "" ).gsub( "}", "" )
+        @current_node[:label] = "#{label}|<#{port}> #{port}"
+      else
+        @current_node[:label] = "<#{port}> #{port}"
+      end
+    end
+    
+    def graphml_graph_edge( node )
+      source = node.attributes['source']
+      source = {source => node.attributes['sourceport']} if node.attributes['sourceport']
+      target = node.attributes['target']
+      target = {target => node.attributes['targetport']} if node.attributes['targetport']
+      
+      @current_edge = @current_graph.add_edge( source, target )
+  
+      node.each_element( ) do |child|
+        #begin
+          send( "graphml_graph_edge_#{child.name}".to_sym, child )
+        #rescue NoMethodError => e
+        #  raise "ERROR node #{child.name} can be child of graphml"
+        #end
+      end
+      
+      @current_edge = nil
+    end
+  
+    def graphml_graph_edge_data( node )
+      @current_edge[@attributs[:edges][node.attributes['key']][:name]] = node.texts().to_s
+    end
+    
+    def graphml_graph_hyperedge( node )
+      list = []
+      
+      node.each_element( ) do |child|
+        if child.name == "endpoint"
+          if child.attributes['port']
+            list << { child.attributes['node'] => child.attributes['port'] }
+          else
+            list << child.attributes['node']
+          end
+        end
+      end
+      
+      list.each { |s|
+        list.each { |t|
+          @current_graph.add_edge( s, t ) unless s == t
+        }
+      } 
+    end
   end
 end
