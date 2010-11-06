8f7bd1a9e33afa5721d7f4d5281c9f1a98aba25d
glejeune <gregoire.lejeune@free.fr>
2010-11-06 14:13:54 +0100

    Update README

--- examples/sample57.rb
+++ examples/sample57.rb
@@ -2,6 +2,7 @@ $:.unshift( "../lib" )
 
 require 'graphviz/graphml'
 
-graphml = File.join( File.dirname( File.expand_path( __FILE__ ) ), "graphml", "cluster.graphml" )
+graphml = File.join( File.dirname( File.expand_path( __FILE__ ) ), "graphml", "port.graphml" )
 g = GraphViz::GraphML.new( graphml )
 g.graph.output( :png => "#{$0}.png" )
+puts g.graph.output( :none => String )
