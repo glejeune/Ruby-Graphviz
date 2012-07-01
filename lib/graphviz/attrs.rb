8c13ac0fcb46a50dbf411bca5a41c62d819a01b9
glejeune <gregoire.lejeune@gmail.com>
2012-07-01 20:48:17 +0200

    Update (c)

--- lib/graphviz/attrs.rb
+++ lib/graphviz/attrs.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010 Gregoire Lejeune <gregoire.lejeune@free.fr>
+# Copyright (C) 2004 - 2012 Gregoire Lejeune <gregoire.lejeune@free.fr>
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
