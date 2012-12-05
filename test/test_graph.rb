b114947990dcba2f85ef0b68bf5f1b1735a557e4
Gabe Kopley <gabe@coshx.com>
2012-12-04 23:25:30 -0800

    Pass the build in Ruby 1.8

--- test/test_graph.rb
+++ test/test_graph.rb
@@ -110,7 +110,7 @@ class GraphVizTest < Test::Unit::TestCase
     assert_nothing_raised 'to_s with edge with numeric label failed.' do
       a = @graph.add_nodes('a')
       b = @graph.add_nodes('b')
-      @graph.add_edges(a, b, label: 5)
+      @graph.add_edges(a, b, :label => 5)
       @graph.to_s
     end
   end
