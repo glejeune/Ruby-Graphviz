6406615f0205f8db4dc3f1adf479a4609ee7fc8e
Kenichi Kamiya <kachick1@gmail.com>
2013-01-12 01:56:54 +0900

    Remove a warning "shadowing outer local variable - g" in examples

--- examples/sample38.rb
+++ examples/sample38.rb
@@ -2,11 +2,11 @@
 $:.unshift( "../lib" )
 require "graphviz"
 
-g = GraphViz::new( :G ) { |g|
-  g.a[:label => "ε"]
-  g.add_nodes( "b", :label => "ε" )
-  g.c[:label => 'ε']
-  g.add_nodes( "d", :label => 'ε' )
+g = GraphViz::new( :G ) { |_g|
+  _g.a[:label => "ε"]
+  _g.add_nodes( "b", :label => "ε" )
+  _g.c[:label => 'ε']
+  _g.add_nodes( "d", :label => 'ε' )
 }
 
 puts g.output( :none => String, :png => "#{$0}.png", :path => "/usr/local/bin" )
